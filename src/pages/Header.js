e5a26c8f5b36b4be34f5c58ed59bedb503986a3d
uziriahimu <uziriahimu@gmail.com>
2022-10-25 17:33:00 +0600

    header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" className='p-3'>
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Navbar.Brand> <Link className='text-white text-decoration-none' to='/'>CSE Courses</Link></Navbar.Brand>
+                    <Nav >
+                        <Nav.Link > <Link className=" text-white text-decoration-none" to='/course'>Courses</Link></Nav.Link>
+                        <Nav.Link >  <Link className=" text-white text-decoration-none" to='/faq'>FAQ</Link></Nav.Link>
+                        <Nav.Link ><Link className=" text-white text-decoration-none" to='/blog'>Blog</Link></Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Blog from "../pages/Blog";
+import Courses from "../pages/Courses";
 import Error from "../pages/Error";
+import Faq from "../pages/Faq";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
         children: [
-
+            {
+                path: '/course',
+                element: <Courses></Courses>,
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>,
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>,
+            }
         ]
     },
     {
